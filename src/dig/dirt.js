63844f96c3f795a604ed4fc0234fb6a7fa637db6
Kyle Whittington <kthomaswhittington@gmail.com>
2015-10-17 21:19:37 -0500

    Add touch event listener to dig.Dirt.
    Closes #8.

--- src/dig/dirt.js
+++ src/dig/dirt.js
@@ -1,5 +1,17 @@
 dig.Dirt = dig.Sprite.extend({
   ctor: function () {
     this._super('res/dirt.png')
+
+    cc.eventManager.addListener({
+      event: cc.EventListener.TOUCH_ALL_AT_ONCE,
+      onTouchesMoved: function (touches, event) {
+        cc.log(touches)
+        var touch = touches[0]
+        var delta = touch.getDelta()
+        var node = event.getCurrentTarget()
+        var diff = cc.pAdd(delta, node.getPosition())
+        node.setPosition(diff)
+      }
+    }, this)
   }
 })
