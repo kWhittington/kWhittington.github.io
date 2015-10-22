45cea412ff70da9d963f8adbf2bbb01178c89521
Kyle Whittington <kthomaswhittington@gmail.com>
2015-10-22 16:14:20 -0500

    Add spaces to dig.Sprite instance methods.

--- src/dig/sprite.js
+++ src/dig/sprite.js
@@ -2,9 +2,11 @@ dig.Sprite = cc.Sprite.extend({
   getLocation: function () {
     return this.getPosition()
   },
+
   intersectsWith: function (point) {
     return cc.pFuzzyEqual(this.getLocation(), point, this.radius())
   },
+
   radius: function () {
     return this.getSpriteFrame().getRect().width / 3
   }
