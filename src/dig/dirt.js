5973fc3ff34e52b18ea918b5984d35a62830ade4
Kyle Whittington <kthomaswhittington@gmail.com>
2015-10-22 17:32:08 -0500

    Add dig.Dirt#worldBoundsIntersectsWith(point).
    dig.Dirt objects are now considered to intersect with a point if it
    falls within their square bounding box.

--- src/dig/dirt.js
+++ src/dig/dirt.js
@@ -1,5 +1,13 @@
 dig.Dirt = dig.Sprite.extend({
   ctor: function () {
     this._super('res/dirt.png')
+  },
+
+  intersectsWith: function (point) {
+    return this.worldBoundsIntersectsWith(point)
+  },
+
+  worldBoundsIntersectsWith: function (point) {
+    return cc.rectContainsPoint(this.getBoundingBoxToWorld(), point)
   }
 })
