1816d64a7eaf06ca41268781601dcd732f779a59
Kyle Whittington <kthomaswhittington@gmail.com>
2015-10-24 19:17:35 -0500

    Add dig.Sprites.Dirt.TAG

--- src/dig/sprites/dirt.js
+++ src/dig/sprites/dirt.js
@@ -2,9 +2,12 @@ dig.Sprites.Dirt = dig.Sprite.extend({
   ctor: function () {
     this._super('res/dirt.png')
     this.setContentSize(cc.size(64, 64))
+    this.setTag(dig.Sprites.Dirt.TAG)
   },
 
   intersectsWithPoint: function (point) {
     return this.worldBoundsIntersectsWithPoint(point)
   }
 })
+
+dig.Sprites.Dirt.TAG = 'dirt'
