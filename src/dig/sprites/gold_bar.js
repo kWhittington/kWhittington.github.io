0b9849e9774d2e6c5a67c7fd7318a3bb0664f96b
Kyle Whittington <kthomaswhittington@gmail.com>
2015-10-24 22:42:44 -0500

    Add dig.Sprites.GoldBar.
    Closes #7.

---
+++ src/dig/sprites/gold_bar.js
@@ -0,0 +1,25 @@
+dig.Sprites.GoldBar = dig.Sprite.extend({
+  ctor: function () {
+    this._super('res/gold_bar_grey.png')
+    this.setContentSize(cc.size(64, 64))
+    this.setTag(dig.Sprites.GoldBar.TAG)
+  },
+
+  getCorrectBinTag: function () {
+    return dig.Sprites.DirtBin.TAG
+  },
+
+  getMultiplier: function () {
+    return 1
+  },
+
+  getScorePoint: function () {
+    return 100
+  },
+
+  getWrongBinTag: function () {
+    return dig.Sprites.GoldBin.TAG
+  }
+})
+
+dig.Sprites.GoldBar.TAG = 'gold_bar'
