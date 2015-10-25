e38188d35f5c408139975e07577df75ecdd198a3
Kyle Whittington <kthomaswhittington@gmail.com>
2015-10-24 19:14:00 -0500

    Add dig.Sprite.DirtBin.TAG and set it during initialization.

--- src/dig/sprites/dirt_bin.js
+++ src/dig/sprites/dirt_bin.js
@@ -2,5 +2,8 @@ dig.Sprites.DirtBin = dig.Sprite.extend({
   ctor: function () {
     this._super('res/dirt_bin.png')
     this.setContentSize(cc.size(192, 384))
+    this.setTag(dig.Sprites.DirtBin.TAG)
   }
 })
+
+dig.Sprites.DirtBin.TAG = 'dirt_bin'
